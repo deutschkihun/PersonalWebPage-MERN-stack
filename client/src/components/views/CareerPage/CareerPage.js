3fd71195473e1b077df354187ce836206e01b191
deutschkihun <deutschkihun@gmail.com>
2020-11-04 11:00:56 +0100

    update2

--- client/src/components/views/CareerPage/CareerPage.js
+++ client/src/components/views/CareerPage/CareerPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button} from 'antd'
+import {Button,Card,Avatar} from 'antd'
 import '../../../main.css'
 import profile from '../common/profile.png'
 import udacity from '../common/udacity.JPG'
@@ -8,6 +8,8 @@ import mintcolleg from '../common/mintcolleg.png'
 import git from '../common/git.png'
 import github from '../common/github.png'
 import javascript from '../common/3stack.png'
+import {BulbFilled} from '@ant-design/icons'
+const {Meta} = Card;
 
 function CareerPage() {
     return (
@@ -57,11 +59,51 @@ function CareerPage() {
                          width='500' height='130'alt="kit"/>  
                         </a>  
                     </div>
-                    <div className=" box-five-1">KIT WIWI </div>
-                    <div className=" box-five-2">Udacity nanodegree</div>
+                    <div className=" box-five-1">
+                        <p style={{display:'inline-flex'}}>Education</p>
+                        <ul style={{textAlign:'left'}}>
+                            <li>Major :Industrial Engineering</li>
+                            <li>Bachelor of science</li>
+                            <li>From Oct. 2017 to present</li>
+                            <li>Advance field : mathemathical optimization & computer science</li>
+                        </ul>
+                    </div>
+                    <div className=" box-five-2">
+                        <p style={{display:'inline-flex'}}>Nanodegree</p>
+                        <ul style={{textAlign:'left'}}>
+                            <li>Data Analyst nanodegree program</li>
+                            <li>Provided by Udacity</li>
+                            <li>From Feb. 2020 to May 2020</li>
+                            <li>Python programing for data analysis with statistic background</li>
+                            <li><a href="https://drive.google.com/file/d/1bSGPpEvvfNC6ZjTMYVsgNm9azys5aT6N/view?usp=sharing" target="_blank">
+                            <Button>See Certificate</Button>
+                        </a></li>
+                        </ul>
+                    </div>
                     <div className=" box-six">WORKING EXPERIENCE</div>
-                    <div className=" box-seven-1">dmTech</div>
-                    <div className=" box-seven-2">mintcolleg</div>
+                    <div className=" box-seven-1">
+                        <p style={{display:'inline-flex'}}>Working Student</p>
+                        <ul style={{textAlign:'left'}}>
+                            <li>Customoer recipt data management</li>
+                            <li>Daily data warehouse controlling with SQL</li>
+                            <li>Debug reporting of data warehouse with SQL</li>
+                            <li><a href="https://drive.google.com/file/d/1hcUIODMNDrFPiHijQib1IXmngAGTFtWB/view?usp=sharing" target="_blank">
+                            <Button>Employment reference</Button>
+                        </a></li>
+                        </ul>
+                    </div>
+                    <div className=" box-seven-2">
+                         <p style={{display:'inline-flex'}}>Math Tutor</p>
+                        <ul style={{textAlign:'left'}}>
+                            <li>Teaching math for computer sciecne students</li>
+                            <li>Explanation of excercise</li>
+                            <li>Lead question and answer session</li>
+                            <li>Lecture field: Integral,Derivative,log & exponential function</li>
+                            <li><a href="https://drive.google.com/file/d/10wy637QRz0jTBr2QOn4LOPpaR5FTH-Uk/view?usp=sharing" target="_blank">
+                            <Button>Employment reference</Button>
+                        </a></li>
+                        </ul>
+                    </div>
 
                     <div className=" box-eight-1">
                         <a href="https://www.dm-jobs.com/dmTECH/?locale=de_DE" target="_blank">
@@ -92,9 +134,40 @@ function CareerPage() {
                         <img src={git} width='130' height='130'alt="kit"/>  
                         <img src={github} width='130' height='130'alt="kit"/>  
                     </div>
-                    <div className=" box-eleven-1">MERN stack</div>
-                    <div className=" box-eleven-2">JS HTML CSS</div>
-                    <div className=" box-eleven-3">Git & Github</div>
+                    <div className=" box-eleven-1">
+                        <p style={{display:'inline-flex'}}>MERN Stack</p>
+                        <ul style={{textAlign:'left'}}>
+                            <li>MERN Stack = Mongo DB & Express JS & React & nodejs</li>
+                            <li>Standard full stack development package</li>
+                            <li>Several projects experience</li>
+                            <li>Skill level : Intermediate </li>
+                            <li><a href="/project">
+                            <Button>See all my MERN stack projects</Button>
+                        </a></li>
+                        </ul>
+                    </div>
+                    <div className=" box-eleven-2">
+                        <p style={{display:'inline-flex'}}>Javascript & HTML & CSS</p>
+                        <ul style={{textAlign:'left'}}>
+                            <li>Fundamental knowledge for Web development</li>
+                            <li>Several projects experience</li>
+                            <li>Skill level : Intermediate </li>
+                            <li><a href="/project">
+                            <Button>See all my related skill projects</Button>
+                        </a></li>
+                        </ul>
+                    </div>
+                    <div className=" box-eleven-3">
+                         <p style={{display:'inline-flex'}}>Git & Github</p>
+                        <ul style={{textAlign:'left'}}>
+                            <li>Most popular distributed version control system</li>
+                            <li>Tracking changes in source code during software development</li>
+                            <li>All my projects are managed by Git and Github</li>
+                            <li><a href="https://github.com/deutschkihun?tab=repositories" target="_blank">
+                            <Button>See my Repository</Button>
+                        </a></li>
+                        </ul>
+                    </div>
 
                 </div>
              <div>
